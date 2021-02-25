a437c6e45ad19556e33b58542fa2f6c9d293062e
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-02-25 18:09:29 +0530

    Done: mongooes hooks, hashing password, auth view, cookies, web token and new user sign up

--- lesson-1/controllers/authController.js
+++ lesson-1/controllers/authController.js
@@ -1,4 +1,5 @@
-const User = require('../models/Users')
+const User = require('../models/Users');
+const jwt = require('jsonwebtoken');
 
 // Handle Errors
 const handelErrors = (err) => {
@@ -24,6 +25,13 @@ const handelErrors = (err) => {
     return errors
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'secret key string', {
+        expiresIn: maxAge
+    });
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -36,13 +44,14 @@ module.exports.login_get = (req, res) => {
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const user = await User.create({email, password})
-        res.status(201).json(user);
+        const user = await User.create({ email, password })
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+        res.status(201).json({ user: user._id });
     } catch (err) {
         const errors = handelErrors(err);
         res.status(400).json({ errors });
     }
-    res.send('new signup')
 }
 
 module.exports.login_post = async (req, res) => {
